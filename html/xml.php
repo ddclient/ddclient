c18902e3add17542c6f83e9a5b3f6ea6ce326723
wimpunk <wimpunk@users.sf.net>
2006-06-20 17:09:06 +0000

    Initial xml.php file
    git-svn-id: svn+ssh://svn.code.sf.net/p/ddclient/code/trunk@15 3873ddee-7413-0410-b6c4-c2c57c1ab35a

---
+++ html/xml.php
@@ -0,0 +1,11 @@
+<?php
+$text['id'] = "intro";
+$text['title'] = "Introduction about the xml page";
+$text['menu']  = "introduction";
+$text['body']  =
+"<p>".
+"This page is just a test about the xml-functionality.  It has \n".
+"nothing special.".
+"</p>";
+$main[] = $text;
+?>
